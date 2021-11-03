e1b2070c861d87b087c88a761a80ff3d872382a2
Maksim Kovtun <brain1999@mail.ru>
2021-11-03 16:24:14 +0300

    create database, users added. Tables modified for PostgreSQL

---
+++ database_structure/_create_database.sql
@@ -0,0 +1,12 @@
+/*
+CREATE DATABASE netomerch;
+
+CREATE USER netomerch_dbadmin WITH PASSWORD '1';
+
+CREATE USER netomerch_dbuser WITH PASSWORD '1';
+
+ALTER DATABASE netomerch OWNER TO netomerch_dbadmin;
+
+\c netomerch netomerch_dbadmin
+
+*/
--- database_structure/database.sql
+++ database_structure/database.sql
@@ -3,7 +3,7 @@
  */
 DROP VIEW  IF EXISTS  custs;
 
-DROP TABLE IF EXISTS  orders CASCADE;
+DROP TABLE IF EXISTS order_contents, orders CASCADE;
 DROP TABLE IF EXISTS cat_order_state CASCADE;
 
 DROP TABLE IF EXISTS basket_items CASCADE;
@@ -27,102 +27,63 @@ DROP TABLE IF EXISTS category CASCADE;
 -- CATEGORY SECTION
 
 CREATE TABLE category(
-	id INT UNSIGNED auto_increment PRIMARY KEY COMMENT 'id',
-	parent_id INT UNSIGNED comment 'link to the parent category, if applicable',
-	name varchar(255) NOT NULL default '' comment 'name of the category',
-	short_description varchar(255) comment 'short description, up to 10 words',
-	description varchar(255) comment 'description of the category',
-	image varchar(255) comment 'link to the image (path to the hosting''s file or URI)'
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COMMENT='category of the items';	
+	id 					serial 			PRIMARY KEY ,
+	parent_id 			INTEGER,
+	category_name 		varchar(255) 	NOT NULL default '' ,
+	short_description 	varchar(255),
+	description 		varchar(255),
+	image 				varchar(255)
+);
+comment on table category is 'category of the items';	
+comment on column category.id is 'id';
+comment on column category.parent_id  is 'link to the parent category, if applicable';
+comment on column category.category_name  is 'name of the category';
+comment on column category.short_description  is 'short description, up to 10 words';
+comment on column category.description  is 'description of the category';
+comment on column category.image  is 'link to the image (path to the hosting''s file or URI)';
+
 -- approximately 4+4+21+101+256+101 = 487 bytes/category = 100 categories (50K)
+grant select on category to netomerch_dbuser;
 
 
 
 
 -- CUSTOMER SECTION
 CREATE TABLE customers(
-	id INT UNSIGNED auto_increment PRIMARY KEY COMMENT 'id increment +1 from 1',
-	first_name varchar(100),
-	last_name varchar(100),
-	password varchar(100),
-	phone varchar(100),
-	email varchar(100),
-	address varchar(100),
-	ip	varchar(100),
-	is_registered bool default False,
-	is_active bool default True
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COMMENT='information about registered customers';
+	id 				SERIAL 			PRIMARY KEY,
+	first_name 		varchar(100),
+	last_name 		varchar(100),
+	password 		varchar(100),
+	phone 			varchar(100),
+	email 			varchar(100),
+	address 		varchar(100),
+	ip				varchar(100),
+	is_registered 	boolean 		default False,
+	is_active 		boolean 		default True
+);
+comment on table customers is 'information about registered customers';
+comment on column customers.id is 'id';
 -- approximately 4+31+31+21+31+11+21+16+1+1 = 168 bytes/user = 10.000 users (1600K)
+grant select, insert, update, delete on customers to netomerch_dbuser;
 
-
-
--- PROPERTY SECTION
-
-CREATE TABLE cat_main_properties(
-	id INT UNSIGNED auto_increment PRIMARY KEY COMMENT 'id',
-	property_type char(1) comment 'type of the property d(ate), s(tring), n(umeric)',
-	name varchar(255) comment 'name of the main property',
-	description varchar(255) comment 'description of the main property (for example, for hint in the admin board)'
-) 
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COMMENT='catalogue of the main properties';	
--- approximately 4+21+201 = 487 bytes/category = 1000 main properties (220K)
 
 CREATE TABLE cat_special_properties(
-	id INT UNSIGNED auto_increment PRIMARY KEY COMMENT 'id',
-	name varchar(255) comment 'name of the special property',
-	description varchar(255) comment 'description of the special property (for example, for hint in the admin board)'
-) 
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COMMENT='catalogue of the special (for items) properties';	
+	id 				SERIAL 			PRIMARY KEY,
+	property_name 	varchar(255),
+	description 	varchar(255)
+);
+comment on table cat_special_properties is 'catalogue of the special (for items) properties';
+comment on column cat_special_properties.id is 'id';
+comment on column cat_special_properties.property_name is 'name of the special property';
+comment on column cat_special_properties.id is 'description of the special property (for example, for hint in the admin board)';
 -- approximately 4+21+201 = 487 bytes/category = 1000 main properties (220K)
 
+grant select on cat_special_properties to netomerch_dbuser;
 
 
 
 
 
-
--- PRODUCT SECTION
-
-CREATE TABLE products(
-	id INT UNSIGNED auto_increment PRIMARY KEY COMMENT 'id',
-	category_id INT UNSIGNED comment 'reference to category',
-	name varchar(255) NOT NULL default '' comment 'name of the product',
-	short_description varchar(255) comment 'short description, up to 10 words',
-	description varchar(255) comment 'description of the product',
-	image varchar(255) comment 'link to the image (path to the hosting''s file or URI)' /* we can CHANGE FOR multiply images*/
-	, CONSTRAINT FOREIGN KEY (category_id) REFERENCES category(id)
-	)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COMMENT='products (items grouping by main properties)';	
--- approximately 4+4+31+51+256+201 = 547 bytes/product = 1000 products (550K)
-
-
-CREATE TABLE product_main_properties(
-	id BIGINT UNSIGNED auto_increment PRIMARY KEY COMMENT 'id',
-	main_property_id INT UNSIGNED comment 'reference to refbook of main properties',
-	product_id INT UNSIGNED comment 'reference to products',
-	d_value DATETIME comment 'value for d type property',
-	s_value varchar(255) comment 'value for s type property',
-	n_value NUMERIC comment 'value for n type property',
-	text varchar(255) comment 'text equivalent for property' /*we can make it AS calculated*/
-	, CONSTRAINT FOREIGN KEY (product_id) REFERENCES products(id)
-	, CONSTRAINT FOREIGN KEY (main_property_id) REFERENCES cat_main_properties(id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COMMENT='table contains values of main properties for product';	
--- approximately 4+4+8+21+4+21 = 62 bytes/main property = 10 properties/produc 1000 products (600K)
 
 
 
@@ -131,104 +92,115 @@ COMMENT='table contains values of main properties for product';
 
 -- ITEM SECTION
 CREATE TABLE items(
-	id INT UNSIGNED auto_increment PRIMARY KEY COMMENT 'id',
-	product_id INT UNSIGNED  comment 'reference to the product',
-	default_price NUMERIC comment 'default price',
-	name varchar(255) NOT NULL comment 'name of the item',
-	short_description varchar(255) comment 'short description, up to 10 words',
-	description varchar(255) comment 'description of the item'
-	, CONSTRAINT FOREIGN KEY (product_id) REFERENCES products(id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COMMENT='items separated by special properties, e.g. 1 product has 2 sizes, therefore it has 2 items';	
+	id 					SERIAL 			PRIMARY KEY,
+	category_id 		INTEGER,
+	default_price 		money,
+	item_name 			varchar(255) 	NOT NULL,
+	short_description 	varchar(255),
+	description 		varchar(255)
+	, CONSTRAINT fk_items_category	FOREIGN KEY (category_id) REFERENCES 	category(id)
+);
+comment on table items is 'items separated by special properties';
+comment on column items.id is 'id';
+comment on column items.category_id is 'reference to the product';
+comment on column items.default_price is 'default price';
+comment on column items.item_name is 'name of the item';
+comment on column items.short_description is 'short description, up to 10 words';
+comment on column items.description is 'description of the item';
+
 -- approximately 4+4+31+101+256 = 396 bytes/item * 100.000 items (39000K)
+grant select on items to netomerch_dbuser;
+
 
 CREATE TABLE item_special_properties(
-	id BIGINT UNSIGNED auto_increment PRIMARY KEY COMMENT 'id',
-	special_property_id INT UNSIGNED comment 'reference to refbook of special properties',
-	item_id INT UNSIGNED comment 'reference to items',
-	d_value DATETIME comment 'value for d type property',
-	s_value varchar(255) comment 'value for s type property',
-	n_value NUMERIC comment 'value for n type property',
-	text varchar(255) comment 'text equivalent for property' /* we can make it AS calculated */
-	, CONSTRAINT FOREIGN KEY (item_id) REFERENCES items(id)
-	, CONSTRAINT FOREIGN KEY (special_property_id) REFERENCES cat_special_properties(id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COMMENT='table contains values of special properties for item';	
+	id 					SERIAL 	PRIMARY KEY,
+	special_property_id INTEGER,
+	item_id 			INTEGER,
+	d_value 			timestamp,
+	s_value 			varchar(255),
+	n_value 			real,
+	text_value 			varchar(255)
+	, CONSTRAINT fk_item_sp_items		FOREIGN KEY (item_id) 				REFERENCES items(id)
+	, CONSTRAINT fk_item_sp_cat_sp		FOREIGN KEY (special_property_id) 	REFERENCES cat_special_properties(id)
+);
+comment on table item_special_properties is 'table contains values of special properties for item';	
+comment on column item_special_properties.id is 'id';
+comment on column item_special_properties.special_property_id is 'reference to refbook of special properties';
+comment on column item_special_properties.item_id is 'reference to items';
+comment on column item_special_properties.d_value is 'value for d type property';
+comment on column item_special_properties.s_value is 'value for s type property';
+comment on column item_special_properties.n_value is 'value for n type property';
+comment on column item_special_properties.text_value is 'text equivalent for property';
 -- approximately 4+4+8+21+4+21 = 62 bytes/special property = 10 properties/item * 100.000 items (60000K)
-
-
--- BACKET SECTION
-
-CREATE TABLE baskets (
-	id BIGINT UNSIGNED auto_increment PRIMARY KEY COMMENT 'id',
-	customer_id INT UNSIGNED comment 'reference to customers'
-	, CONSTRAINT FOREIGN KEY (customer_id) REFERENCES customers(id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COMMENT='baskets info';
--- approximately 8+4+4+4 = 18 bytes/basketspecial property = 10 properties/item * 100.000 items (60000K)
-
-CREATE TABLE basket_items (
-	id BIGINT UNSIGNED auto_increment PRIMARY KEY COMMENT 'id',
-	basket_id BIGINT UNSIGNED  comment 'reference to baskets',
-	item_id INT UNSIGNED COMMENT 'reference to items',
-	amount INT comment 'amount of item',
-	discount_program INT comment 'field for future feature :)',
-	total_sum_for_items NUMERIC comment 'total sum of item*amout - discount'
-	, CONSTRAINT FOREIGN KEY (item_id)  REFERENCES items(id)
-	, CONSTRAINT FOREIGN KEY (basket_id)  REFERENCES baskets(id)
-	
-	
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COMMENT='basket with items';	
--- approximately 8+8+4+4+4+4 = 32 bytes/item = 10 items in 1 baskets * 100.000 baskets (31200K)
-
+grant select on item_special_properties to netomerch_dbuser;
 
 
 
 -- ORDER SECTION
 
 CREATE TABLE cat_order_state(
-	id INT UNSIGNED auto_increment PRIMARY KEY COMMENT 'id',
-	name varchar(100) not null comment 'name of an order''s state, for example - paying',
-	description varchar(255) comment 'description of an order''s state'
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COMMENT='reference book of order states';
+	id 			SERIAL 			PRIMARY KEY,
+	state_name 	varchar(100) 	not null,
+	description varchar(255) 
+);
+comment on table cat_order_state is 'reference book of order states';
+comment on column cat_order_state.id is 'id';
+comment on column cat_order_state.state_name is 'name of an order''s state, for example - paying';
+comment on column cat_order_state.description is 'description of an order''s state';
+
 -- approximately 4+11+201 = 216 bytes/state = 10 states (2K)
+grant select on cat_order_state to netomerch_dbuser;
 
 
 CREATE TABLE orders (
-	id BIGINT UNSIGNED auto_increment PRIMARY KEY COMMENT 'id',
-	order_created_at datetime NOT NULL comment 'datetime of creation an order',
-	order_finished_at datetime NOT NULL comment 'datetime of finishing an order',
-	order_state_id int UNSIGNED comment 'reference to order_state', 
-	order_updated_at datetime NOT NULL comment 'datetime of updating an order',
-	basket_id BIGINT UNSIGNED comment 'reference to backet',
-	discount_program int comment 'field for future feature :)',
-	customer_id INT UNSIGNED COMMENT 'reference to customers ',
-	cust_address varchar(255) COMMENT 'delivery address if applicable',
-	cust_phone varchar(100) comment 'cusomer''s phone if applicable',
-	cust_email varchar(100) comment 'customer''s e-mail if applicable',
-	order_delivery_at datetime comment 'delivery date and time if applicable',
-	total_sum NUMERIC comment 'total sum of an order'
-	, CONSTRAINT FOREIGN KEY (order_state_id)  REFERENCES cat_order_state(id)
-	, CONSTRAINT FOREIGN KEY (basket_id)  REFERENCES  baskets(id) 
-	, CONSTRAINT FOREIGN KEY (customer_id)  REFERENCES  customers(id) 
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COMMENT='order''s information';	
+	id 					serial 			PRIMARY KEY,
+	uid 				varchar(100), 
+	order_created_at 	timestamp 		NOT NULL,
+	order_finished_at 	timestamp 		NOT NULL,
+	order_state_id 		INTEGER, 
+	order_updated_at 	timestamp 		NOT NULL,
+	discount_program 	INTEGER,
+	customer_id 		INTEGER,
+	cust_address 		varchar(255),
+	cust_phone 			varchar(100),
+	cust_email 			varchar(100),
+	order_delivery_at 	timestamp,
+	total_sum 			money
+	, CONSTRAINT fk_orders_cat_os		FOREIGN KEY (order_state_id)  	REFERENCES 	cat_order_state(id)
+	, CONSTRAINT fk_orders_customers	FOREIGN KEY (customer_id)  		REFERENCES  customers(id) 
+);
+comment on table orders is 'order''s information';	
+comment on column orders.id is 'id';
+comment on column orders.uid is 'unique identifier of order';
+comment on column orders.order_created_at is 'timestamp of creation an order';
+comment on column orders.order_finished_at is 'timestamp of finishing an order';
+comment on column orders.order_state_id is 'reference to order_state';
+comment on column orders.order_updated_at is 'timestamp of the last update of the order';
+comment on column orders.discount_program is 'field for future feature :)';
+comment on column orders.customer_id is 'reference to customers';
+comment on column orders.cust_address is 'delivery address if applicable';
+comment on column orders.cust_phone is 'cusomer''s phone if applicable';
+comment on column orders.cust_email is 'customer''s e-mail if applicable';
+comment on column orders.order_delivery_at is 'delivery date and time if applicable';
+comment on column orders.total_sum is 'total sum of the order';
 -- approximately 8+8+4+4+201+11+21+8 = 265 bytes/order = 100.000 orders (25800K)
+grant select, insert, update, delete on orders to netomerch_dbuser;
 
+create table order_contents(
+	id 			serial 		PRIMARY KEY,
+	order_id	integer 	NOT NULL,
+	item_id		integer		NOT null
+	, constraint fk_order_conts_orders foreign key (order_id) references orders(id)
+	, constraint fk_order_conts_items foreign key (item_id) references items(id)	
+);
+comment on table order_contents is 'order contents';
+comment on column order_contents.id is 'id';
+comment on column order_contents.order_id is 'referece to orders';
+comment on column order_contents.item_id is 'reference to items';
+
+-- approximately 4+4+4 = 12 bytes/item in order = 100.000 orders * 10 items in order (11700K)
+grant select, insert, update, delete on orders to netomerch_dbuser;
+	
 
 
 
