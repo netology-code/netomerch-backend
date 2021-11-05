553051f7f0e921af973b39a1232fb11ec6e7ec29
n.pronichev <n.pronichev@macbook-n.pronichev>
2021-11-05 15:19:26 +0300

    Complete middleware for GET API

--- neto_merch/shop/middleware.py
+++ neto_merch/shop/middleware.py
@@ -1,10 +1,7 @@
-from django.http import JsonResponse
-from django.shortcuts import redirect, reverse
-from rest_framework.request import Request
+from django.http import HttpResponse
 from rest_framework import status
-from .views import ProductViewSet, CategoryViewSet
 from django.core.cache import cache
-from django.conf import settings
+import json
 
 
 class CacheMethodsMiddleware:
@@ -14,30 +11,23 @@ class CacheMethodsMiddleware:
     def __call__(self, request):
         self.path = request.path_info.lstrip('/').split('/')[2]
         if self.path == 'categories':
-            categories = cache.get('category')
-            print(categories)
-            return JsonResponse(categories, status=status.HTTP_200_OK)
-        response = self.get_response(request)
-        return response
-
-    def process_view(self, request, view_func, view_args, view_kwargs):
-
-        if self.path == 'categories':
-            if 'category' in cache:
-                categories = cache.get('category')
-                print(categories)
-                return JsonResponse(categories, status=status.HTTP_200_OK)
-            # else:
-            # TODO: тут надо как-то вызвать нашу функцию Get из класса CategoryViewSet, но как это сделать - я хз
-            # TODO: был вариант сделать Redirect, но он не сильно помогает, т.к. не знает такого пути
-            # TODO: Прямой вызов тоже не помогает. Если решить этот вопрос - все заработает
+            if self.path in cache:
+                if cache.get(self.path) is not None:
+                    categories = json.loads(cache.get(self.path))
+                    return HttpResponse(categories, status=status.HTTP_200_OK)
+            else:
+                response = self.get_response(request)
+                print(json.dumps(response.data))
+                cache.set(self.path, json.dumps(response.data))
+                return response
 
         elif self.path == 'items':
-            if 'items' in cache:
-                products = cache.get('items')
-                return JsonResponse(products, status=status.HTTP_200_OK)
-
-            # else:
-                # TODO: Аналогично If, который выше
-        else:
-            return None
+            if self.path in cache:
+                if cache.get(self.path) is not None:
+                    categories = json.loads(cache.get(self.path))
+                    return HttpResponse(categories, status=status.HTTP_200_OK)
+            else:
+                response = self.get_response(request)
+                print(json.dumps(response.data))
+                cache.set(self.path, json.dumps(response.data))
+                return response
