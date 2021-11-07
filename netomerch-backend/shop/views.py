e42bd6927a19d74f31ebbcc15f4fb6224a083169
n.pronichev <n.pronichev@macbook-n.pronichev>
2021-11-07 23:11:26 +0300

    Delete all depends code

--- netomerch-backend/shop/views.py
+++ netomerch-backend/shop/views.py
@@ -1,25 +1,25 @@
-from rest_framework.viewsets import ModelViewSet
-
-from .serializers import *
-from .models import *
-from .permissions import *
-
-
-class BaseViewSet:
-    """только админ может удалять, обновлять, создавать, просматривать могут все"""
-    # def get_permissions(self):
-    #     """Получение прав для действий"""
-    #
-    #     # TODO: здесь показывает предупреждение, но всё ок, но наследование ModelViewSet нельзя сюда в класс добавить
-    #     if self.action in ['create', 'destroy', 'update', 'partial_update']:
-    #         return [IsAdmin()]
-    #     return []
-    pass
+# from rest_framework.viewsets import ModelViewSet
+#
+# from .serializers import *
+# from .models import *
+# from .permissions import *
 #
 #
-class CategoryViewSet(BaseViewSet, ModelViewSet):
-    pass
+# class BaseViewSet:
+#     """только админ может удалять, обновлять, создавать, просматривать могут все"""
+#     def get_permissions(self):
+#         """Получение прав для действий"""
 #
-#
-class ProductViewSet(BaseViewSet, ModelViewSet):
-    pass
+#         # TODO: здесь показывает предупреждение, но всё ок, но наследование ModelViewSet нельзя сюда в класс добавить
+#         if self.action in ['create', 'destroy', 'update', 'partial_update']:
+#             return [IsAdmin()]
+#         return []
+#     pass
+# #
+# #
+# class CategoryViewSet(BaseViewSet, ModelViewSet):
+#     pass
+# #
+# #
+# class ProductViewSet(BaseViewSet, ModelViewSet):
+#     pass
