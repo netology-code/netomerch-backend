1b3b12f6f41cf22c0883eddaf9c82d24b9ce630a
n.pronichev <n.pronichev@macbook-n.pronichev>
2021-11-07 19:00:57 +0300

    Change middleware

--- netomerch-backend/shop/middleware.py
+++ netomerch-backend/shop/middleware.py
@@ -9,12 +9,18 @@ class CacheMethodsMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        self.path = request.path_info.lstrip('/').split('/')[2]
-        if self.path in cache:
-            if cache.get(self.path) is not None:
+        self.path = request.path_info.lstrip('/').split('/')
+        if 'api' not in self.path or len(self.path) != 4:
+            response = self.get_response(request)
+            return response
+        else:
+            self.path = self.path[2]
+            if self.path in cache.keys('*') and json.loads(cache.get(self.path)) != []:
                 resp = json.loads(cache.get(self.path))
                 return HttpResponse(resp, status=status.HTTP_200_OK)
-        else:
-            response = self.get_response(request)
-            cache.set(self.path, json.dumps(response.data))
-            return response
+
+            else:
+                response = self.get_response(request)
+                if json.dumps(response.data) != []:
+                    cache.set(self.path, json.dumps(response.data))
+                return response
