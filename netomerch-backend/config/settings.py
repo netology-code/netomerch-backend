e31b33fefa3cbde3b381c7a2137b67286fc17ed4
mikepro-alfamail-ru <mikepro@alfamail.ru>
2021-11-07 18:04:08 +0300

    Add env variables to workflow. Docker-compose: Change hostnames of containers (add mode), mod backend network - make external.

--- netomerch-backend/config/settings.py
+++ netomerch-backend/config/settings.py
@@ -19,7 +19,6 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -131,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = f'{BASE_DIR}/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- netomerch-backend/run.sh
+++ netomerch-backend/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 cd /netomerch-backend
-python manage.py migrate --no-input
+bash ./wait.sh db-postgres:5432 -- python manage.py migrate --no-input
+
 gunicorn --bind 0.0.0.0:8000 config.wsgi
