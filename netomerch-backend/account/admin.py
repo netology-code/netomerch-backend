996e6a9eee2de29d7eff479c3d7cd307c23cfbef
Maksim Kovtun <brain1999@mail.ru>
2021-11-08 14:35:25 +0300

    account admin.py added

--- netomerch-backend/account/admin.py
+++ netomerch-backend/account/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from account.models import Customer, Address
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
 # Register your models here.
+
+
+class AddressInline(admin.TabularInline):
+    model = Address
+
+
+class UserAdmin(DefaultUserAdmin):
+    model = Customer
+    filter_horizontal = ('addresses',)
+
+    list_display = (
+        'username', 'first_name', 'last_name', 'email', 'phone', 'is_superuser', 'is_staff', 'is_active',
+    )
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {
+         'fields': (('first_name', 'last_name'), 'email', 'phone', 'addresses')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+
+admin.site.register(Address)
+admin.site.register(Customers, UserAdmin)
