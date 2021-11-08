e0e87ecdea1bf830312f29e697bf5e91498ba547
Maksim Kovtun <brain1999@mail.ru>
2021-11-08 13:10:15 +0300

    fields in Address model added

--- netomerch-backend/account/models.py
+++ netomerch-backend/account/models.py
@@ -6,7 +6,15 @@ from django.db.models.deletion import SET_DEFAULT
 
 
 class Address(models.Model):
-    address = models.TextField()
+    country = models.TextField(max_length=100, blank=True)
+    region = models.TextField(max_length=100, blank=True)
+    city = models.TextField(max_length=100, blank=True)
+    street = models.TextField(max_length=100, blank=True)
+    house = models.TextField(max_length=10, blank=True)
+    appartment = models.TextField(max_length=10, blank=True)
+    extra_info = models.TextField(max_length=255, blank=True)
+
+    address = models.TextField(blank=True)
 
     def __str__(self):
         return self.address
@@ -21,4 +29,4 @@ class Customer(AbstractUser):
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.first_name} {self.last_name} {email}"
