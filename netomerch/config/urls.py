4e11523ebd68beada0e6c99a51bd08677c06655f
mikepro-alfamail-ru <mikepro@alfamail.ru>
2021-11-14 00:20:21 +0300

    fix urls.py

--- netomerch/config/urls.py
+++ netomerch/config/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from config.api_docs import urlpatterns as api_docs_urlpatterns
 from django.contrib import admin
 from django.urls import include, path
+
+from config.api_docs import urlpatterns as api_docs_urlpatterns
 
 urlpatterns = api_docs_urlpatterns + [
     path("admin/", admin.site.urls),
