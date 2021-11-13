d649fe2d8fac65931fa880f60f8fda1ad9c3878a
Maksim Kovtun <brain1999@mail.ru>
2021-11-13 15:58:56 +0300

    Django-taggit added, app products modified

--- netomerch/apps/products/admin.py
+++ netomerch/apps/products/admin.py
@@ -25,7 +25,7 @@ class CategoryAdmin(admin.ModelAdmin):
         return super().get_queryset(request).prefetch_related('tags')
 
     def tag_list(self, obj):
-        return u", ".join(o.name for o in obj.tags.all())
+        return ", ".join(o.name for o in obj.tags.all())
 
 
 class ItemAdmin(admin.ModelAdmin):
