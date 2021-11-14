7b32bb969f402dba8091e9e6ccef841db5137fae
mikepro-alfamail-ru <mikepro@alfamail.ru>
2021-11-14 22:10:58 +0300

    fix https in swagger, fix rights in ci

--- netomerch/config/settings.py
+++ netomerch/config/settings.py
@@ -173,6 +173,9 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 20,  # количество элементов на одной странице для пагинации
 }
 
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 SWAGGER_SETTINGS = {
     'LOGIN_URL': '/admin/login/',
     'LOGOUT_URL': '/admin/logout/',
