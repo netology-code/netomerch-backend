ea06c294a4c131108007835c905521389edc885f
mikepro-alfamail-ru <mikepro@alfamail.ru>
2021-11-22 19:56:24 +0300

    Prepare docker-compose.yml for celery

--- netomerch/apps/email/models.py
+++ netomerch/apps/email/models.py
@@ -4,7 +4,8 @@ from django.utils.translation import gettext_lazy as _
 
 class EmailTemplate(models.Model):
     class Meta:
-        verbose_name_plural = _("Templates")
+        verbose_name = _("Шаблон")
+        verbose_name_plural = _("Шаблоны")
 
     id = models.CharField(max_length=20, primary_key=True)
     description = models.CharField(max_length=100, blank=True, null=True)
