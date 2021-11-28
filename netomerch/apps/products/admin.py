7169ea043df98aa7e1f920b8d4102401a1570b59
n.pronichev <n.pronichev@macbook-n.pronichev>
2021-11-28 23:38:49 +0300

    little changes for products admin

--- netomerch/apps/products/admin.py
+++ netomerch/apps/products/admin.py
@@ -1,44 +1,53 @@
 from django.contrib import admin
 from django.db import models
+from django.utils.safestring import mark_safe
 from django_json_widget.widgets import JSONEditorWidget
 
 from apps.products.models import Category, Image, Item, ItemProperty
 
 
-class ImageAdmin(admin.StackedInline):
+class ItemImageAdmin(admin.StackedInline):
     model = Image.items.through
     extra = 1
 
 
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     model = Category
     formfield_overrides = {
         models.JSONField: {'widget': JSONEditorWidget},
     }
 
+    list_display = ("name", "short_description", "description", "cat_photo")
 
-admin.site.register(Category, CategoryAdmin)
+    def cat_photo(self, obj):
+        if obj.image:
+            return mark_safe(f"<img src='{obj.image.url}' width=50>")
 
 
+@admin.register(ItemProperty)
 class ItemPropertyAdmin(admin.ModelAdmin):
     model = ItemProperty
 
+    list_display = ("name", "type", "description")
 
-admin.site.register(ItemProperty, ItemPropertyAdmin)
 
-
+@admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    inlines = [ImageAdmin]
+    inlines = [ItemImageAdmin]
 
     exclude = ['image']
+
+    list_display = ("name", "categories", "price", "short_description")
 
     model = Item
     formfield_overrides = {
         models.JSONField: {'widget': JSONEditorWidget},
     }
 
-
-admin.site.register(Item, ItemAdmin)
+    def categories(self, obj):
+        categories = Category.objects.filter(item__pk=obj.id).values_list('name', flat=True)
+        return ', '.join(categories)
 
 
 @admin.register(Image)
