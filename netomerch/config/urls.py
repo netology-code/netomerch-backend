d08487f00c67d16e8be545bf2b54c7bdf643e3cf
Mikhail Prozorov <80358549+mikepro-alfamail-ru@users.noreply.github.com>
2021-12-08 11:28:24 +0300

    Repair celery finally, move api path to constant API (#75)
    * Repair celery finally, move api path to constant API
    
    * Fix cicd

--- netomerch/config/urls.py
+++ netomerch/config/urls.py
@@ -5,13 +5,15 @@ from django.urls import include, path
 
 from config.api_docs import urlpatterns as api_docs_urlpatterns
 
+APIV1 = "api/v1/"
+
 urlpatterns = api_docs_urlpatterns + [
     path("admin/", admin.site.urls),
-    path("api/v1/", include("apps.products.urls")),
-    path("api/v1/", include("apps.orders.urls")),
-    path("api/v1/", include("apps.reviews.urls")),
-    path("api/v1/task/", include("apps.taskqueue.urls")),
-    path("callback/", include("apps.email.urls")),
+    path(APIV1, include("apps.products.urls")),
+    path(APIV1, include("apps.orders.urls")),
+    path(APIV1, include("apps.reviews.urls")),
+    path(f"{APIV1}task/", include("apps.taskqueue.urls")),
+    path(f"{APIV1}callback/", include("apps.email.urls")),
     path('summernote/', include('django_summernote.urls')),
 ]
 
