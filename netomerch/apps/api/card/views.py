f7aef271fab92564e7d6dfec45a82a731aa8bc2e
Maksim Kovtun <brain1999@mail.ru>
2021-12-16 21:27:32 +0300

    cardContract is ready to PR

--- netomerch/apps/api/card/views.py
+++ netomerch/apps/api/card/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import mixins
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import ViewSet
 
 from apps.products.models import Item
 
 from .serializers import cardSerializer
 
 
-class cardEndPointView(mixins.RetrieveModelMixin, GenericViewSet):
+class cardEndPointView(ViewSet):
     """
     end-point of card /api/v1/card/<int>/
     Everybody can
@@ -16,18 +15,8 @@ class cardEndPointView(mixins.RetrieveModelMixin, GenericViewSet):
     User can see only is_published=True card
     """
 
-    def get_queryset(self, pk):
-        if self.request.user.is_superuser:
-            queryset = Item.objects.filter(pk=pk).prefetch_related("onitem").all()
-        else:
-            queryset = Item.objects.filter(pk=pk, is_published=True).prefetch_related("onitem").all()
-        return queryset
-
-    def retrieve(self, request, *args, **kwargs):
-        print(kwargs)
-
-        pk = kwargs['pk']
-        queryset = self.get_queryset(pk)
-
-        serializer_class = cardSerializer(dict(items=queryset), context={"request": request})
-        return Response(serializer_class.data)
+    def retrieve(self, request, pk=None):
+        queryset = Item.objects.filter(is_published=True).all()
+        item = get_object_or_404(queryset, pk=pk)
+        serializer = cardSerializer(item, context={"request": request})
+        return Response(serializer.data)
