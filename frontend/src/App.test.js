82cb6d1f35751c838e693897322b28a5146d6841
Mikhail Prozorov <mikepro@alfamail.ru>
2021-12-18 01:47:42 +0300

    test frontend cicd

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders text App', () => {
-  render(<App />);
+  store = mockStore(initialState);
+  const { getByText } = render(
+      <Provider store={store}>
+          <App />
+      </Provider>
+  );
   expect(screen.getByText('Hello World!!!')).toBeInTheDocument();
 });
