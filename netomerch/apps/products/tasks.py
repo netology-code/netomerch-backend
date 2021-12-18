1484522e2a22817e1419fc7f4e5e4e150a984ebf
Mikhail Prozorov <mikepro@alfamail.ru>
2021-12-18 23:17:24 +0300

    bugfix and change downloader form g.drive

--- netomerch/apps/products/tasks.py
+++ netomerch/apps/products/tasks.py
@@ -1,5 +1,7 @@
 from celery.utils.log import get_task_logger
-from google_drive_downloader import GoogleDriveDownloader as gdd
+import gdown
+import imghdr
+import os
 
 from apps.products.models import ImageColorItem
 from apps.taskqueue.celery import app
@@ -7,11 +9,13 @@ from apps.taskqueue.celery import app
 logger = get_task_logger(__name__)
 
 
-@app.task(max_retries=3, rate_limit="3/m")
+@app.task(max_retries=3, rate_limit="10/m")
 def download_image(id, g_url):
     db_image_color = ImageColorItem.objects.get(id=id)
-    file_id = g_url.split('/')[-2]
-    gdd.download_file_from_google_drive(file_id, dest_path=f'media/item/{db_image_color.id}image.jpg')
-    db_image_color.image = f'/item/{db_image_color.id}image.jpg'
+    filename = f'media/item/{db_image_color.id}'
+    gdown.download(g_url, fuzzy=True, output=filename)
+    ext = imghdr.what(filename)
+    os.rename(filename, f'{filename}.{ext}')
+    db_image_color.image = f'/item/{db_image_color.id}.{ext}'
     db_image_color.save()
     pass
