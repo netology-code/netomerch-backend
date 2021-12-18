884dca480cdccfdbafbd52fbe77f61f5149ced8e
Mikhail Prozorov <mikepro@alfamail.ru>
2021-12-18 20:15:09 +0300

    refactor management command (was too complicated)

--- netomerch/apps/products/management/commands/load_products.py
+++ netomerch/apps/products/management/commands/load_products.py
@@ -6,7 +6,7 @@ from apps.products.tasks import download_image
 
 
 def parse_xlsx(filename):
-    items = []
+    items = dict()
 
     wb = load_workbook(filename)
     sheet = wb[wb.sheetnames[0]]
@@ -18,22 +18,29 @@ def parse_xlsx(filename):
         if not sheet.cell(column=1, row=row).value:
             break
         default_color = sheet.cell(column=8, row=row).value is not None
-        items.append({'name': sheet.cell(column=1, row=row).value})
-        items[-1]['category'] = sheet.cell(column=2, row=row).value
-        items[-1]['specialization'] = sheet.cell(column=3, row=row).value
-        items[-1]['sizes'] = [size.strip() for size in sheet.cell(column=4, row=row).value.split(',')]
-        items[-1]['colors'] = []
+        name = sheet.cell(column=1, row=row).value
+        specialization = sheet.cell(column=3, row=row).value
+        if (name, specialization) not in items:
+            items[(name, specialization)] = {
+                "name": name,
+                "specialization": specialization,
+                "colors": [],
+            }
+        items[(name, specialization)]['category'] = sheet.cell(column=2, row=row).value
+        items[(name, specialization)]['sizes'] = [
+            size.strip() for size in sheet.cell(column=4, row=row).value.split(',')
+        ]
         color_tuple = [color.strip() for color in sheet.cell(column=5, row=row).value.split(',')]
-        items[-1]['colors'].append({
+        items[(name, specialization)]['colors'].append({
             'color': {'name': color_tuple[0], 'code': color_tuple[1]},
             'images': [sheet.cell(column=j, row=row).value for j in range(9, 13)],
             'default': default_color
         })
-        items[-1]['description'] = sheet.cell(column=6, row=row).value
-        items[-1]['short_description'] = sheet.cell(column=7, row=row).value
-        items[-1]['is_hit'] = sheet.cell(column=14, row=row).value is not None
+        items[(name, specialization)]['description'] = sheet.cell(column=6, row=row).value
+        items[(name, specialization)]['short_description'] = sheet.cell(column=7, row=row).value
+        items[(name, specialization)]['is_hit'] = sheet.cell(column=14, row=row).value is not None
         price = sheet.cell(column=15, row=row).value
-        items[-1]['price'] = float(price) if price is not None else 0.0
+        items[(name, specialization)]['price'] = float(price) if price is not None else 0.0
 
         row += 1
     return items
@@ -47,8 +54,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         items = parse_xlsx(options['filename'][0])
-        loaded_items = set()
-        for item in items:
+        for _, item in items.items():
             category = Category.objects.get_or_create(name=item['category'])[0]
 
             specialization = Specialization.objects.get_or_create(name=item['specialization'])[0]
@@ -57,18 +63,17 @@ class Command(BaseCommand):
                                                  category=category,
                                                  specialization=specialization)
             db_item = db_item[0]
-            if db_item not in loaded_items:
-                db_item.description = item['description']
-                db_item.short_description = item['short_description']
 
-                db_item.price = item['price']
-                db_item.is_hit = item['is_hit']
-                db_item.save()
+            db_item.description = item['description']
+            db_item.short_description = item['short_description']
 
-                image_color_items_to_delete = ImageColorItem.objects.filter(item=db_item)
-                image_color_items_to_delete.delete()
-                db_item.size.all().delete()
-                loaded_items.add(db_item)
+            db_item.price = item['price']
+            db_item.is_hit = item['is_hit']
+            db_item.save()
+
+            image_color_items_to_delete = ImageColorItem.objects.filter(item=db_item)
+            image_color_items_to_delete.delete()
+            db_item.size.all().delete()
 
             for size in item['sizes']:
                 db_size = Size.objects.get_or_create(name=size.capitalize())
