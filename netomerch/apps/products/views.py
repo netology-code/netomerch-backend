ae0d9675d30734f2c6d5a0965dae5abeca9ac0cc
Mikhail Prozorov <80358549+mikepro-alfamail-ru@users.noreply.github.com>
2021-12-22 09:42:23 +0300

    command rewritten to be able to load data from currrent xlsx doc (#94)
    * command rewritten to be able to load data from currrent xlsx doc
    
    * add remove_unused_images management command

--- netomerch/apps/products/views.py
+++ netomerch/apps/products/views.py
@@ -1,9 +1,9 @@
+# from apps.email.tasks import sendmail
 from django.conf import settings
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from rest_framework.viewsets import ModelViewSet
 
-# from apps.email.tasks import sendmail
 from apps.products.models import Category, Item
 from apps.products.permissions import IsAdmin
 from apps.products.serializers import CategorySerializer, ItemSerializer
